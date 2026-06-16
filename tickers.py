9748023b0c3cb47ccafbd07aeb373a2e98735d14
certhz-maker <certhz@gmail.com>
2026-06-16 11:24:48 +0200

    Update tickers.py

--- tickers.py
+++ tickers.py
@@ -6,7 +6,7 @@ TICKERS = [
     # --- spazio ---
     "ASTS", "FLY", "PL", "SIDU", "BKSY", "SATL", "SPIR", "VOYG", "VELO", "MNTS",
     # --- uranio ---
-    "OKLO", "SMR", "UUUU",
+    "OKLO", "SMR", "UUUU", "JOBY", "ACHR",
     # --- quantum ---
     "LAES", "IONQ", "QBTS", "ARQQ", "QUBT", "QSI", "ATOM",
     # --- tecnologici ---
